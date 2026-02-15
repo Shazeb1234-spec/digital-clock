aaa668a540090a415edc9bebff7b3556755eef1a
Shazeb <Shazeb.arafin@gmail.com>
2026-02-15 23:24:39 +0400

    clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,51 @@
 "use client"
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { useState } from "react"
+import { Badge } from "@/components/ui/badge"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+import { useEffect, useState } from "react"
 
 export default function Page() {
     const [date, setDate] = useState(() => new Date())
-    setInterval(() => {
+    useEffect(()=>{
+        const intervalId = setInterval(() => {
         setDate(new Date())
     }, 1000)
+
+    return () => clearInterval(intervalId)
+    })
+    
     const hour = date.getHours()
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
     return (
-        <div>
-            <Card className="">
+        <div className="my-10 flex flex-col items-center">
+            <Card size="sm" className="mx-auto w-full max-w-sm">
                 <CardHeader className="justify-center items-center">
                     <CardTitle className="text-3xl font-sans">Clock</CardTitle>
                 </CardHeader>
-                    <div className="justify-center items-center">
-                        <h1 className="text-2xl"> {hour}:{minutes}:{seconds} </h1>
-                        <h2 className="text-sm">{date.toDateString()}</h2>
-                    </div>
+                <CardContent className="">
+
+                    <span>
+                        <Badge className="bg-blue-50 text-blue-700 dark:bg-blue-950 dark:text-blue-300 text-2xl h-8 w-8">
+                            {hour}
+                        </Badge>:
+
+                          <Badge className="bg-blue-50 text-blue-700 dark:bg-blue-950 dark:text-blue-300 text-2xl h-8 w-8">
+                            {minutes}
+                        </Badge>:
+
+                         <Badge className="bg-blue-50 text-blue-700 dark:bg-blue-950 dark:text-blue-300 text-2xl h-8 w-8">
+                            {seconds}
+                        </Badge>
+                    
+                    </span>
+                </CardContent>
+                <CardFooter>
+
+                    <h2 className="text-sm">{date.toDateString()}</h2>
+
+                </CardFooter>
             </Card>
         </div>
     )
